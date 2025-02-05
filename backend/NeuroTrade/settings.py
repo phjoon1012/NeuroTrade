942450f986f6d23a6bcafcf967121a833a3961cb
Hyeonjoon Park <phjoon@umich.edu>
2025-02-05 17:45:11 +0900

    제발 되나

--- backend/NeuroTrade/settings.py
+++ backend/NeuroTrade/settings.py
@@ -43,8 +43,8 @@ CSRF_TRUSTED_ORIGINS = [
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
         'APP': {
-            'client_id': os.getenv('GOOGLE_CLIENT_ID'),
-            'secret': os.getenv('GOOGLE_CLIENT_SECRET'),
+            # 'client_id': os.getenv('GOOGLE_CLIENT_ID'),
+            # 'secret': os.getenv('GOOGLE_CLIENT_SECRET'),
             'key': ''
         },
         'SCOPE': [
