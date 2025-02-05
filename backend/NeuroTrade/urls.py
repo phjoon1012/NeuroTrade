68e4e4a23f81b6c810a70c1d539a1555a787337a
syofbb26 <cube1426@gmail.com>
2025-02-05 14:57:05 +0900

    recent_Trade

--- backend/NeuroTrade/urls.py
+++ backend/NeuroTrade/urls.py
@@ -17,11 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
+urlpatterns = [ 
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),          # Include user routes
     # path('api/dashboard/', include('dashboard.urls')),
-     path('api/dashboard/', include('dashboard.urls')),
+    path('api/dashboard/', include('dashboard.urls')),
     path('api/strategies/', include('strategies.urls')),
     path('api/backtesting/', include('backtesting.urls')),
 ]
