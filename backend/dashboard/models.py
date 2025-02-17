5adf6d14180b3365feb64ad1615dbb85608ba298
hseonjo <coco7kiti@gmail.com>
2025-02-17 21:07:30 +0900

    feat: dashboard 기능 구현 (회원정보 수정 및 API 키 암호화)

--- backend/dashboard/models.py
+++ backend/dashboard/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+# users/models.py에서 User 모델을 가져옵니다.
+# 현재 AUTH_USER_MODEL에 설정된 사용자 모델을 가져옵니다.
+User = get_user_model()  
